b24f39a702f995804f230c7746926b59ad8a661f
edokaaa <ptredoka@gmail.com>
2026-01-21 13:31:05 +0100

    feat: docker prod config

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -14,7 +14,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
 
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      ignoreExpiration: configService.get('ENV') !== 'production',
+      ignoreExpiration: configService.get('NODE_ENV') !== 'production',
       algorithms: ['RS256'],
       secretOrKey: publicKey,
     });
